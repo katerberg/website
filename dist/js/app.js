d13ff8bc20d9571e027e36a718c50648e20a3423
Mark Katerberg <katerberg@fastmail.fm>
2017-09-11 22:26:56 -0500

    Rebuild

--- dist/js/app.js
+++ dist/js/app.js
@@ -80,6 +80,145 @@ katerbergApp.config(["$routeProvider", "$httpProvider", function($routeProvider,
 })();
 
 (function() {
+    angular.module('katerbergApp').factory('pathfinderService', ["$http", "$q", "spellService", function($http, $q, spellService) {
+        function getSpellbook() {
+            return $http.get('static/spells.json').then(function(res) {
+                return res.data.map(function(dirtySpell) {
+                    return spellService.spellify(dirtySpell);
+                });
+            });
+
+        }
+        var casterTypes = ['Bard',
+            'Cleric',
+            'Druid',
+            'Paladin',
+            'Ranger',
+            'Sorcerer/Wizard'];
+
+        return {
+            getSpellbook: getSpellbook,
+            casterTypes: casterTypes
+        };
+    }]);
+})();
+
+
+(function() {
+    'use strict';
+
+    angular.module('katerbergApp')
+    .controller('PathfinderSpellbookCtrl', ["$scope", "pathfinderService", "modalService", function($scope, pathfinderService, modalService) {
+        pathfinderService.getSpellbook().then(function(data) {
+            $scope.spellbook = data;
+        });
+
+        $scope.casterTypes = pathfinderService.casterTypes;
+        $scope.$watch('search.level', function(item) {
+            if (item === null) {
+                $scope.search.level = undefined;
+            }
+        });
+
+        function drillIntoSpell(spell) {
+            modalService.open({
+                templateUrl: modalService.spell.url,
+                controller: modalService.spell.controller,
+                resolve: {spell: function() {
+                    return spell;
+                }}
+            });
+        }
+
+        $scope.drillIntoSpell = drillIntoSpell;
+    }]);
+})();
+
+(function() {
+    angular.module('katerbergApp').factory('Spell', function() {
+        var Spell;
+
+        Spell = (function() {
+            function buildClasses(input) {
+                if (!input) {
+                    return input;
+                }
+                return input.split(', ').map(function(levelInput) {
+                    var classAndLevel = levelInput.split(' ');
+                    classAndLevel[0] = classAndLevel[0].charAt(0).toUpperCase() + classAndLevel[0].substr(1);
+                    classAndLevel[0] = classAndLevel[0].replace(/\/(.)/, function(charToReplace) {
+                        return charToReplace.toUpperCase();
+                    });
+                    return {name: classAndLevel[0], level: parseInt(classAndLevel[1])};
+                });
+            }
+
+            function Spell(args) {
+                if (!args) {
+                    return;
+                }
+                this.description = args.description;
+                this.school = args.school;
+                this.name = args.name;
+                this.classes = buildClasses(args.level);
+                this.area = args.area;
+                this.effect = args.effect;
+                this.descriptor = args.descriptor;
+                this.range = args.range;
+                this.components = args.components;
+                this.duration = args.duration;
+                this.subSchool = args.subschool;
+                this.target = args.target;
+                // jscs:disable requireCamelCaseOrUpperCaseIdentifiers
+                this.savingThrow = args.saving_throw;
+                this.spellResistance = args.spell_resistance;
+                this.shortDescription = args.short_description;
+                this.castingTime = args.casting_time;
+                // jscs:enable requireCamelCaseOrUpperCaseIdentifiers
+            }
+
+            return Spell;
+        })();
+
+        return Spell;
+    });
+})();
+
+
+(function() {
+    'use strict';
+
+    angular.module('katerbergApp').controller('SpellModalCtrl', ["$scope", "$modalInstance", "spell", function($scope, $modalInstance, spell) {
+        function close() {
+            $modalInstance.close();
+        }
+
+        function showDetails(spell) {
+            spell.showDetails = true;
+        }
+
+        $scope.close = close;
+        $scope.spell = spell;
+        $scope.showDetails = showDetails;
+    }]);
+})();
+
+(function() {
+    angular.module('katerbergApp').factory('spellService', ["Spell", function(Spell) {
+        function spellify(dirtySpell) {
+            Object.keys(dirtySpell.fields, function(key, value) {
+                dirtySpell.fields[key] = value.replace(/<[^>]*>/gm, '');
+            });
+            return new Spell(dirtySpell.fields);
+        }
+
+        return {
+            spellify: spellify
+        };
+    }]);
+})();
+
+(function() {
     'use strict';
 
     angular.module('katerbergApp').controller('MinesweeperCtrl', ["$scope", "minesweeperService", function($scope, minesweeperService) {
@@ -226,143 +365,4 @@ katerbergApp.config(["$routeProvider", "$httpProvider", function($routeProvider,
             checkForWin: checkForWin
         };
     });
-})();
-
-(function() {
-    angular.module('katerbergApp').factory('pathfinderService', ["$http", "$q", "spellService", function($http, $q, spellService) {
-        function getSpellbook() {
-            return $http.get('static/spells.json').then(function(res) {
-                return res.data.map(function(dirtySpell) {
-                    return spellService.spellify(dirtySpell);
-                });
-            });
-
-        }
-        var casterTypes = ['Bard',
-            'Cleric',
-            'Druid',
-            'Paladin',
-            'Ranger',
-            'Sorcerer/Wizard'];
-
-        return {
-            getSpellbook: getSpellbook,
-            casterTypes: casterTypes
-        };
-    }]);
-})();
-
-
-(function() {
-    'use strict';
-
-    angular.module('katerbergApp')
-    .controller('PathfinderSpellbookCtrl', ["$scope", "pathfinderService", "modalService", function($scope, pathfinderService, modalService) {
-        pathfinderService.getSpellbook().then(function(data) {
-            $scope.spellbook = data;
-        });
-
-        $scope.casterTypes = pathfinderService.casterTypes;
-        $scope.$watch('search.level', function(item) {
-            if (item === null) {
-                $scope.search.level = undefined;
-            }
-        });
-
-        function drillIntoSpell(spell) {
-            modalService.open({
-                templateUrl: modalService.spell.url,
-                controller: modalService.spell.controller,
-                resolve: {spell: function() {
-                    return spell;
-                }}
-            });
-        }
-
-        $scope.drillIntoSpell = drillIntoSpell;
-    }]);
-})();
-
-(function() {
-    angular.module('katerbergApp').factory('Spell', function() {
-        var Spell;
-
-        Spell = (function() {
-            function buildClasses(input) {
-                if (!input) {
-                    return input;
-                }
-                return input.split(', ').map(function(levelInput) {
-                    var classAndLevel = levelInput.split(' ');
-                    classAndLevel[0] = classAndLevel[0].charAt(0).toUpperCase() + classAndLevel[0].substr(1);
-                    classAndLevel[0] = classAndLevel[0].replace(/\/(.)/, function(charToReplace) {
-                        return charToReplace.toUpperCase();
-                    });
-                    return {name: classAndLevel[0], level: parseInt(classAndLevel[1])};
-                });
-            }
-
-            function Spell(args) {
-                if (!args) {
-                    return;
-                }
-                this.description = args.description;
-                this.school = args.school;
-                this.name = args.name;
-                this.classes = buildClasses(args.level);
-                this.area = args.area;
-                this.effect = args.effect;
-                this.descriptor = args.descriptor;
-                this.range = args.range;
-                this.components = args.components;
-                this.duration = args.duration;
-                this.subSchool = args.subschool;
-                this.target = args.target;
-                // jscs:disable requireCamelCaseOrUpperCaseIdentifiers
-                this.savingThrow = args.saving_throw;
-                this.spellResistance = args.spell_resistance;
-                this.shortDescription = args.short_description;
-                this.castingTime = args.casting_time;
-                // jscs:enable requireCamelCaseOrUpperCaseIdentifiers
-            }
-
-            return Spell;
-        })();
-
-        return Spell;
-    });
-})();
-
-
-(function() {
-    'use strict';
-
-    angular.module('katerbergApp').controller('SpellModalCtrl', ["$scope", "$modalInstance", "spell", function($scope, $modalInstance, spell) {
-        function close() {
-            $modalInstance.close();
-        }
-
-        function showDetails(spell) {
-            spell.showDetails = true;
-        }
-
-        $scope.close = close;
-        $scope.spell = spell;
-        $scope.showDetails = showDetails;
-    }]);
-})();
-
-(function() {
-    angular.module('katerbergApp').factory('spellService', ["Spell", function(Spell) {
-        function spellify(dirtySpell) {
-            Object.keys(dirtySpell.fields, function(key, value) {
-                dirtySpell.fields[key] = value.replace(/<[^>]*>/gm, '');
-            });
-            return new Spell(dirtySpell.fields);
-        }
-
-        return {
-            spellify: spellify
-        };
-    }]);
 })();
