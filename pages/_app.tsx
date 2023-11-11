162837e9cc2c2e46658c5402030baef43a9769b5
Mark Katerberg <katerberg@fastmail.fm>
2023-11-10 20:24:39 -0600

    MTG decks

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,17 @@
 import Layout from '../components/layout';
 import '../styles/globals.scss';
 import type { AppProps } from 'next/app';
+import Script from 'next/script';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <Script id="moxfield">
+        {
+          'function moxfieldOnMessage(e){const t=e.data;if("object"==typeof t&&"moxfield"===t.type){const e=document.getElementById(t.id);e&&(e.style.height=t.data+"px")}}function moxfieldOnLoad(e){e.target&&e.target.contentWindow&&e.target.contentWindow.postMessage({type:"moxfield",data:e.target.id},"*")}window.addEventListener("message",moxfieldOnMessage);'
+        }
+      </Script>
     </Layout>
   );
 }
--- pages/mtg.tsx
+++ pages/mtg.tsx
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { SyntheticEvent, useCallback } from 'react';
 
 function Mtg() {
+  const moxfieldInit = useCallback(
+    (event: SyntheticEvent<HTMLIFrameElement, Event>) => {
+      // eslint-disable-next-line no-undef -- Moxfield is global
+      setTimeout(() => moxfieldOnLoad(event), 10);
+    },
+    []
+  );
+
   return (
     <>
       <h1>{'Magic: the Gathering'}</h1>
@@ -9,25 +17,19 @@ function Mtg() {
           'I used to play Magic semi-competitively. I now play casually. Here are the decks I still have that I enjoy playing:'
         }
       </p>
-      <h2>{'Modern'}</h2>
-      <ul>
-        <li>
-          <a href="http://tappedout.net/mtg-decks/10-09-17-gifts-storm">
-            {'Storm'}
-          </a>
-        </li>
-      </ul>
       <h2>{'Legacy'}</h2>
       <ul>
         <li>
-          <a href="https://tappedout.net/mtg-decks/meandeck-doomsday-2018-07-05-1">
+          <a href="https://www.moxfield.com/decks/ufydTUHb1ka9S1zYD-78Gw">
             {'Doomsday'}
           </a>
-        </li>
-        <li>
-          <a href="http://tappedout.net/mtg-decks/12-04-15-1-land-belcher/">
-            {'1 Land Belcher'}
-          </a>
+          <iframe
+            id="legacy-doomsday"
+            style={{ border: 'none' }}
+            onLoad={moxfieldInit}
+            src="https://www.moxfield.com/embed/ufydTUHb1ka9S1zYD-78Gw"
+            width="100%"
+          />
         </li>
       </ul>
       <h2>{'EDH/Commander'}</h2>
@@ -36,21 +38,52 @@ function Mtg() {
           <a href="http://tappedout.net/mtg-decks/land-edh-5-color/">
             {'5 Color Lands'}
           </a>
+          <iframe
+            id="edh-lands"
+            style={{ border: 'none' }}
+            onLoad={moxfieldInit}
+            src="https://www.moxfield.com/embed/HAWXT6-JmUKNcdsWu9Ebxg"
+            width="100%"
+          />
         </li>
         <li>
-          <a href="http://tappedout.net/mtg-decks/phelddagrif-true-group-hug-edh/">
+          <a href="https://www.moxfield.com/decks/Bk3ZF7TC1kGOY_lBZzc3LA">
             {'Phelddagrif Group Hug'}
           </a>
-        </li>
-        <li>
-          <a href="https://tappedout.net/mtg-decks/11-08-21-lazav-the-multifarious">
-            {'Lazav Reanimator'}
-          </a>
+          <iframe
+            id="edh-phelddagrif"
+            style={{ border: 'none' }}
+            onLoad={moxfieldInit}
+            src="https://www.moxfield.com/embed/Bk3ZF7TC1kGOY_lBZzc3LA"
+            width="100%"
+          />
         </li>
         <li>
           <a href="https://www.moxfield.com/decks/HUC4WcumRka3EHcKHAivvg">
             {'Glissa Recursion'}
           </a>
+          <iframe
+            id="edh-phelddagrif"
+            style={{ border: 'none' }}
+            onLoad={moxfieldInit}
+            src="https://www.moxfield.com/embed/HUC4WcumRka3EHcKHAivvg"
+            width="100%"
+          />
+        </li>
+      </ul>
+      <h2>{'Modern'}</h2>
+      <ul>
+        <li>
+          <a href="https://www.moxfield.com/decks/2NvM2kjEYEW7JD0-XaxqMg">
+            {'Storm'}
+          </a>
+          <iframe
+            id="modern-storm"
+            style={{ border: 'none' }}
+            onLoad={moxfieldInit}
+            src="https://www.moxfield.com/embed/2NvM2kjEYEW7JD0-XaxqMg"
+            width="100%"
+          />
         </li>
       </ul>
       <h2>{'Cubes'}</h2>
