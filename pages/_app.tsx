7b4593bae6956a7c73a5c8daebe881cd81a232ec
Mark Katerberg <katerberg@fastmail.fm>
2026-06-03 20:01:01 -0500

    Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,10 @@
 import Layout from '../components/layout';
 import '../styles/globals.scss';
 import type { AppProps } from 'next/app';
+import { GoogleAnalytics } from '@next/third-parties/google';
 import Script from 'next/script';
+
+const gaId = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -12,6 +15,9 @@ export default function MyApp({ Component, pageProps }: AppProps) {
           'function moxfieldOnMessage(e){const t=e.data;if("object"==typeof t&&"moxfield"===t.type){const e=document.getElementById(t.id);e&&(e.style.height=t.data+"px")}}function moxfieldOnLoad(e){e.target&&e.target.contentWindow&&e.target.contentWindow.postMessage({type:"moxfield",data:e.target.id},"*")}window.addEventListener("message",moxfieldOnMessage);'
         }
       </Script>
+      {process.env.NODE_ENV === 'production' && gaId ? (
+        <GoogleAnalytics gaId={gaId} />
+      ) : null}
     </Layout>
   );
 }
